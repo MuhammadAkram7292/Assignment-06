b7b3713d85dc8f213a6d673ab60face8f671d9ac
MuhammadAkram7292 <121758916+MuhammadAkram7292@users.noreply.github.com>
2023-03-09 20:29:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// CHAPTER : STRING METHOD
+// Question : 03 ;
+
+// var letter = prompt("Enter Your Nationality");
+// var result = letter.indexOf("n");
+// document.write(result);
+
+// Question : 04 ;
+// var letter = prompt("Enter Text");
+// var result = letter.lastIndexOf("l");
+// document.write(result);
+
+// Question : 08 ;
